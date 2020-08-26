7c3f18a13fe8048657f35b22a632b622d426154d
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-08-26 15:02:34 -0500

    modify iconButton

--- src/components/atoms/IconButton.jsx
+++ src/components/atoms/IconButton.jsx
@@ -3,13 +3,22 @@ import React from 'react';
 import Icon from './Icon';
 
 const IconButton = (props) => {
-  const { iconName, size, colorIcon, handleClick, bgColor, space, type } = props;
+  const {
+    iconName, size, colorIcon, handleClick, bgColor, space, type,
+  } = props;
   return (
-    <button 
-        onClick={handleClick} 
-        style={{backgroundColor: ` ${bgColor} `, padding : `${space}`, boxShadow: `var(--Box-Shadow-no-color) ${bgColor}`}} 
-        className={`icon-button icon-button--${type}`}>
-        <Icon iconName={iconName} size={size} color={colorIcon} />
+    <button
+      type="button"
+      onClick={handleClick}
+      style={{
+        backgroundColor: ` ${bgColor} `,
+        width: `${space}`,
+        height: `${space}`,
+        boxShadow: `var(--Box-Shadow-no-color) ${bgColor}`,
+      }}
+      className={`icon-button icon-button--${type}`}
+    >
+      <Icon iconName={iconName} size={size} color={colorIcon} />
     </button>
   );
 };
