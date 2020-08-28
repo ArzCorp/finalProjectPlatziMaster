d25a2b22214a21bf27565e706d7165e80812d8ea
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-08-28 10:16:07 -0500

    add filterModal organism

---
+++ src/components/organisms/FilterModal.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Modal from './Modal';
+
+import Button from '../atoms/Button';
+import Select from '../atoms/Select';
+
+const FilterModal = ({ isClose, isOpenModal, buttonName }) => (
+  <Modal
+    isOpenModal={isOpenModal}
+    isClose={isClose}
+    type="isCloseEdit"
+  >
+
+  <div className="filter-modal">
+
+    <div>
+
+      <h1>¿Qué estás buscando?</h1>
+      <br /> 
+      <p>Elige las opciones para encontrar tu próxima prenda</p>
+      <br /> 
+
+      <Select label="Categoría" opt1="Camisas" opt2="Jeans" opt3="Zapatos" />
+      <Select label="Género" />
+      <Select label="Marca" />
+      <Select label="Estado" />
+      <Select label="Talla" />
+      <Select label="Color" />
+
+    </div>
+ 
+    <Button
+      type="normal"
+      name="filtrar"
+    />
+
+  </div>
+  
+  </Modal>
+);
+
+export default FilterModal;
