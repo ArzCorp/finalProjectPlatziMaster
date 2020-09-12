def099076d21815a786eb58b44cfd6e4253f6fe2
JuanGF <jugaybre@gmail.com>
2020-09-12 02:02:41 -0500

    improve graph design in login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,27 +5,20 @@ import LoginForm from '../components/organisms/LoginForm';
 import PrivacyPolicy from '../components/atoms/PrivacyPolicy';
 
 const Login = () => (
-  <div className="loginPage">
-    <section className="wrapper loginPage__brand">
-      <div className="row loginPage__brand-entry">
-        <div className="column-6">
+  <section className="loginPage">
+    <div className="row login">
+      <div className="column-6">
+        <div className="login__logo">
           <img src={Logo} alt="Logotipo" />
         </div>
+        <h1>Iniciar Sesión</h1>
+        <LoginForm />
       </div>
-      <div className="row loginPage__loginForm">
-        <div className="column-6">
-          <LoginForm />
-        </div>
+      <div className="row login__privacyPolicy">
+        <PrivacyPolicy />
       </div>
-    </section>
-    <section>
-      <div className="wrapper">
-        <div className="row">
-          <PrivacyPolicy />
-        </div>
-      </div>
-    </section>
-  </div>
+    </div>
+  </section>
 );
 
 export default Login;
