6612ce618393aea7d8385dab5494250eac6264e9
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-09-14 11:19:02 -0500

    add privacy policy

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -1,17 +1,253 @@
-import React from 'react';
+import React from "react";
 
 const PrivacyPolicy = () => (
   <div className="privacyPolicy_page">
     <h1>Politica de privacidad</h1>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna
-      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      Duis aute irure dolor in reprehenderit in voluptate velit
-      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-      occaecat cupidatat non proident, sunt in culpa qui officia
-      deserunt mollit anim id est laborum.
+      Ourclothe te informa sobre su Política de Privacidad respecto del
+      tratamiento y protección de los datos de carácter personal de los usuarios
+      y clientes que puedan ser recabados por la navegación o contratación de
+      servicios a través del sitio Web. En este sentido, Ourclothe garantiza el
+      cumplimiento de la normativa vigente en materia de protección de datos
+      personales, reflejada en la Ley Orgánica 3/2018, de 5 de diciembre, de
+      Protección de Datos Personales y de Garantía de Derechos Digitales (LOPD
+      GDD). Cumple también con el Reglamento (UE) 2016/679 del Parlamento
+      Europeo y del Consejo de 27 de abril de 2016 relativo a la protección de
+      las personas físicas (RGPD). El uso de sitio Web implica la aceptación de
+      esta Política de Privacidad así como las condiciones incluidas en el Aviso
+      Legal.
+    </p>
+    <br />
+    <br />
+    <h2>Principios aplicados en el tratamiento de datos</h2>
+    <br />
+
+    <p>
+      En el tratamiento de tus datos personales, Ourclothe aplicará los
+      siguientes principios que se ajustan a las exigencias del nuevo reglamento
+      europeo de protección de datos:
+    </p>
+    <br />
+
+    <ul>
+      <li>
+        Principio de licitud, lealtad y transparencia: Ourclothe siempre
+        requerirá el consentimiento para el tratamiento de tus datos personales
+        que puede ser para uno o varios fines específicos sobre los que te
+        informará previamente con absoluta transparencia.
+      </li>
+      <li>
+        Principio de minimización de datos: Ourclothe te solicitará solo los
+        datos estrictamente necesarios para el fin o los fines que los solicita.
+      </li>
+      <li>
+        Principio de limitación del plazo de conservación: Los datos se
+        mantendrán durante el tiempo estrictamente necesario para el fin o los
+        fines del tratamiento. Ourclothe te informará del plazo de conservación
+        correspondiente según la finalidad. En el caso de suscripciones, el
+        Titular revisará periódicamente las listas y eliminará aquellos
+        registros inactivos durante un tiempo considerable.
+      </li>
+      <li>
+        Principio de integridad y confidencialidad: Tus datos serán tratados de
+        tal manera que su seguridad, confidencialidad e integridad esté
+        garantizada. Debes saber que Ourclothe toma las precauciones necesarias
+        para evitar el acceso no autorizado o uso indebido de los datos de sus
+        usuarios por parte de terceros.
+      </li>
+    </ul>
+    <br />
+    <br />
+    <h2>Obtención de datos personales</h2>
+    <br />
+    <p>
+      Para navegar por Ourclothe no es necesario que facilites ningún dato
+      personal. Los casos en los que sí proporcionas tus datos personales son
+      los siguientes:
+    </p>
+    <br />
+    <ul>
+      <li>Al registrarte a través de los formularios de contacto</li>
+    </ul>
+    <br />
+    <br />
+    <h2>Tus derechos</h2>
+    <br />
+    <p>
+      Ourclothe te informa que sobre tus datos personales tienes derecho a:
+    </p>
+    <br />
+    <ul>
+      <li>Solicitar el acceso a los datos almacenados.</li>
+      <li>Solicitar una rectificación o la cancelación.</li>
+      <li>Solicitar la limitación de su tratamiento.</li>
+      <li>Oponerte al tratamiento.</li>
+      <li>Solicitar la portabilidad de tus datos.</li>
+    </ul>
+    <br />
+    <p>
+      El ejercicio de estos derechos es personal y por tanto debe ser ejercido
+      directamente por el interesado, solicitándolo directamente al Titular, lo
+      que significa que cualquier cliente, suscriptor o colaborador que haya
+      facilitado sus datos en algún momento puede dirigirse al Titular y pedir
+      información sobre los datos que tiene almacenados y cómo los ha obtenido,
+      solicitar la rectificación de los mismos, solicitar la portabilidad de sus
+      datos personales, oponerse al tratamiento, limitar su uso o solicitar la
+      cancelación de esos datos en los ficheros de Ourclothe.
+    </p>
+    <p>
+      Para ejercitar tus derechos de acceso, rectificación, cancelación,
+      portabilidad y oposición tienes que enviar un correo electrónico a
+      info@ourclothe.com junto con la prueba válida en derecho como una
+      fotocopia del D.N.I. o equivalente.
+    </p>
+    <p>
+      Tienes derecho a la tutela judicial efectiva y a presentar una reclamación
+      ante la autoridad de control, en este caso, la Agencia Española de
+      Protección de Datos, si consideras que el tratamiento de datos personales
+      que te conciernen infringe el Reglamento.
+    </p>
+    <br />
+    <br />
+    <h2>Finalidad del tratamiento de datos personales</h2>
+    <br />
+    <p>
+      Cuando te conectas al sitio Web para mandar un correo al Titular, te
+      suscribes a su boletín o realizas una contratación, estás facilitando
+      información de carácter personal de la que el responsable es Ourclothe.
+      Esta información puede incluir datos de carácter personal como pueden ser
+      tu dirección IP, nombre y apellidos, dirección física, dirección de correo
+      electrónico, número de teléfono, y otra información. Al facilitar esta
+      información, das tu consentimiento para que tu información sea recopilada,
+      utilizada, gestionada y almacenada por superadmin.es , sólo como se
+      describe en el Aviso Legal y en la presente Política de Privacidad.
+    </p>
+    <p>
+      Los datos personales y la finalidad del tratamiento por parte de Ourclothe
+      es diferente según el sistema de captura de información:
+    </p>
+    <br />
+    <ul>
+      <li>
+        Formularios de contacto: Ourclothe solicita datos personales entre los
+        que pueden estar: Nombre y apellidos, dirección de correo electrónico,
+        número de teléfono y dirección de tu sitio Web con la finalidad de
+        responder a tus consultas. Por ejemplo, Ourclothe utiliza esos datos
+        para dar respuesta a tus mensajes, dudas, quejas, comentarios o
+        inquietudes que puedas tener relativas a la información incluida en el
+        sitio Web, los servicios que se prestan a través del sitio Web, el
+        tratamiento de tus datos personales, cuestiones referentes a los textos
+        legales incluidos en el sitio Web, así como cualquier otra consulta que
+        puedas tener y que no esté sujeta a las condiciones del sitio Web o de
+        la contratación.
+      </li>
+    </ul>
+    <br />
+    <br />
+    <h2>Seguridad de los datos personales</h2>
+    <br />
+    <p>
+      Para proteger tus datos personales, Ourclothe toma todas las precauciones
+      razonables y sigue las mejores prácticas de la industria para evitar su
+      pérdida, mal uso, acceso indebido, divulgación, alteración o destrucción
+      de los mismos. El sitio Web está alojado en Amazon Web Services. La
+      seguridad de tus datos está garantizada, ya que toman todas las medidas de
+      seguridad necesarias para ello. Puedes consultar su política de privacidad
+      para tener más información.
+    </p>
+    <br />
+    <br />
+    <h2>Contenido de otros sitios web</h2>
+    <br />
+    <p>
+      Las páginas de este sitio Web pueden incluir contenido incrustado (por
+      ejemplo, vídeos, imágenes, artículos, etc.). El contenido incrustado de
+      otras web se comporta exactamente de la misma manera que si hubieras
+      visitado la otra web. Estos sitios Web pueden recopilar datos sobre ti,
+      utilizar cookies, incrustar un código de seguimiento adicional de
+      terceros, y supervisar tu interacción usando este código.
+    </p>
+    <br />
+    <br />
+    <h2>Legitimación para el tratamiento de datos</h2>
+    <br />
+
+    <p>La base legal para el tratamiento de tus datos es: el consentimiento.</p>
+    <p>
+      Para contactar con Ourclothe, suscribirte a un boletín o realizar
+      comentarios en este sitio Web tienes que aceptar la presente Política de
+      Privacidad.
+    </p>
+    <br />
+    <br />
+    <h2>Categorías de datos personales</h2>
+    <br />
+    <p>Las categorías de datos personales que trata Ourclothe son:</p>
+    <br />
+    <ul>
+      <li>Datos identificativos.</li>
+    </ul>
+    <br />
+    <br />
+    <h2>Conservación de datos personales</h2>
+    <br />
+    <p>
+      Los datos personales que proporciones al Titular se conservarán hasta que
+      solicites su supresión.
+    </p>
+    <br />
+    <br />
+    <h2>Exactitud y veracidad de los datos personales</h2>
+    <br />
+    <p>
+      Te comprometes a que los datos facilitados al Titular sean correctos,
+      completos, exactos y vigentes, así como a mantenerlos debidamente
+      actualizados.
+    </p>
+    <p>
+      Como Usuario del sitio Web eres el único responsable de la veracidad y
+      corrección de los datos que remitas al sitio exonerando a Ourclothe de
+      cualquier responsabilidad al respecto.
+    </p>
+
+    <br />
+    <br />
+    <h2>Aceptación y consentimiento</h2>
+    <br />
+    <p>
+      Como Usuario del sitio Web declaras haber sido informado de las
+      condiciones sobre protección de datos de carácter personal, aceptas y
+      consientes el tratamiento de los mismos por parte de Ourclothe en la
+      forma y para las finalidades indicadas en esta Política de Privacidad.
+    </p>
+
+    <br />
+    <br />
+    <h2>Revocabilidad</h2>
+    <br />
+    <p>
+      Para ejercitar tus derechos de acceso, rectificación, cancelación,
+      portabilidad y oposición tienes que enviar un correo electrónico a
+      info@ourclothe.com junto con la prueba válida en derecho como una
+      fotocopia del D.N.I. o equivalente.
+    </p>
+    <p>
+      El ejercicio de tus derechos no incluye ningún dato que Ourclothe esté
+      obligado a conservar con fines administrativos, legales o de seguridad.
+    </p>
+
+    <br />
+    <br />
+    <h2>Cambios en la Política de Privacidad</h2>
+    <br />
+    <p>
+      Ourclothe se reserva el derecho a modificar la presente Política de
+      Privacidad para adaptarla a novedades legislativas o jurisprudenciales,
+      así como a prácticas de la industria.
+    </p>
+    <p>
+      Estas políticas estarán vigentes hasta que sean modificadas por otras
+      debidamente publicadas.
     </p>
   </div>
 );
